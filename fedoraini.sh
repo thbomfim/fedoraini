0212514f246bcda6fd5e5fde2d3704dc9658f68f
Thiago Bomfim <thiagobomfimofc@gmail.com>
2025-10-10 12:57:28 -0300

    Create fedoraini.sh
    Co-Authored-By: Thiago Bomfim <40004059+thbomfim@users.noreply.github.com>

---
+++ fedoraini.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo dnf upgrade --refresh -y
+sudo dnf install \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf install vlc -y
